ab45c98fcd7da503d0812596e030b37903c2f052
Markus Opolka <markus.opolka@netways.de>
2023-04-06 16:57:56 +0200

    Extend unittests

--- main_test.go
+++ main_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestMainAlerts_matches(t *testing.T) {
+func TestMainCheck_matches(t *testing.T) {
 	testcases := map[string]struct {
 		input    string
 		regex    []string
@@ -26,6 +26,78 @@ func TestMainAlerts_matches(t *testing.T) {
 	for name, tc := range testcases {
 		t.Run(name, func(t *testing.T) {
 			actual := matches(tc.input, tc.regex)
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestMainAlerts_GetPerfdata(t *testing.T) {
+	testcases := map[string]struct {
+		ao       AlertOverview
+		expected string
+	}{
+		"simple-overview": {
+			ao:       AlertOverview{},
+			expected: "'alerts'=0 'alerts_high'=0 'alerts_medium'=0 'alerts_low'=0",
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			actual := tc.ao.GetPerfdata()
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestMainAlerts_GetOutput(t *testing.T) {
+	testcases := map[string]struct {
+		ao       AlertOverview
+		expected string
+	}{
+		"simple-overview": {
+			ao: AlertOverview{
+				Total:  1,
+				Output: []string{"test"},
+			},
+			expected: "\n## Alerts\ntest\n",
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			actual := tc.ao.GetOutput()
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
+
+func TestMainAlerts_GetStatus(t *testing.T) {
+	testcases := map[string]struct {
+		ao       AlertOverview
+		expected int
+	}{
+		"simple-overview": {
+			ao:       AlertOverview{},
+			expected: 0,
+		},
+		"simple-warning": {
+			ao: AlertOverview{
+				Medium: 1,
+			},
+			expected: 1,
+		},
+		"simple-critical": {
+			ao: AlertOverview{
+				High: 1,
+			},
+			expected: 2,
+		},
+	}
+
+	for name, tc := range testcases {
+		t.Run(name, func(t *testing.T) {
+			actual := tc.ao.GetStatus()
 			assert.Equal(t, tc.expected, actual)
 		})
 	}
